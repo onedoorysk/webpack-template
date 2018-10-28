3ae2084e2757ad829063a549b93b607840957036
onedoorysk <rysk1008@gmail.com>
2018-10-28 19:30:23 +0900

    create webpack template with react

---
+++ webpack-with-react/src/components/List.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './List.module.css'
+
+export default () => {
+  return (
+    <ul className={styles.list}>
+      <li>A</li>
+      <li>B</li>
+      <li>C</li>
+    </ul>
+  )
+}
---
+++ webpack-with-react/webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env',
+                '@babel/preset-react'
+              ]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader?modules']
+      }
+    ]
+  }
+}
